9913106681a42feb653671d3b56c11ab2bc14369
yaonam <elimviolinist@gmail.com>
2023-09-23 09:03:01 -0400

    added chain configs

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -1,8 +1,58 @@
 package utils
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
 type ChainConfig struct {
 	RPCEndpoint string            `json:"rpcEndpoint"`
 	Tokens      map[string]string `json:"tokens"`
 }
 
 var ChainConfigs = make(map[string]*ChainConfig)
+
+func init() {
+	// Parse all config json files
+	_, filename, _, _ := runtime.Caller(0)
+	currentDir := filepath.Dir(filename)
+	configPath := filepath.Join(currentDir, "configs")
+	configDir, _ := os.ReadDir(configPath)
+	for _, config := range configDir {
+		log.Printf("Loading %v...", config.Name())
+		if err := loadConfig(config.Name()); err != nil {
+			log.Printf("Failed to load %v: %v", config.Name(), err)
+		}
+	}
+}
+
+// Loads the chain config including rpc endpoint and token mappings.
+func loadConfig(configName string) error {
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	path := filepath.Join(dir, "configs", configName)
+	rawConfig, err := os.Open(path)
+	if err != nil {
+		log.Printf("Failed to open config: %v", err)
+		return err
+	}
+	defer rawConfig.Close()
+	var parsedConfig ChainConfig
+	readConfig, err := io.ReadAll(rawConfig)
+	if err != nil {
+		log.Printf("Failed to read config: %v", err)
+	}
+	err = json.Unmarshal(readConfig, &parsedConfig)
+	if err != nil {
+		log.Printf("Failed to parse config: %v", err)
+		return err
+	}
+
+	ChainConfigs[strings.TrimSuffix(configName, ".json")] = &parsedConfig
+	return nil
+}
