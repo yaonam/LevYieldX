212b369da209290323893af0a0e885484dacd086
yaonam <elimviolinist@gmail.com>
2023-09-22 22:43:56 -0400

    Added protocols interface and types

--- cmd/protocols/protocols.go
+++ cmd/protocols/protocols.go
@@ -1,3 +1,12 @@
 package protocols
 
+import "github.com/ethereum/go-ethereum/core/types"
+
 // Interface/handler for different protocols (Aave, Compound)
+
+type YieldProtocol interface {
+	// Get all markets
+	GetMarkets() []*MarketInfo
+	// Returns the transactions required to execute the strategy step.
+	GetTransactions(wallet string, step *StrategyStep) ([]*types.Transaction, error)
+}
---
+++ cmd/protocols/schema.go
@@ -0,0 +1,33 @@
+package protocols
+
+import "math/big"
+
+type ProtocolChain struct {
+	Protocol      string        `json:"protocol"`
+	Chain         string        `json:"chain"`
+	SupplyMarkets []*MarketInfo `json:"lendingSpecs"`
+	BorrowMarkets []*MarketInfo `json:"borrowingSpecs"`
+}
+
+type MarketInfo struct {
+	Chain      string   `json:"chain"`
+	Protocol   string   `json:"protocol"`
+	Market     string   `json:"market"`
+	Token      string   `json:"token"`
+	Decimals   *big.Int `json:"decimals"`
+	LTV        *big.Int `json:"ltv"`        // In basis points, 0 if cannot be collateral
+	PriceInUSD *big.Int `json:"priceInUSD"` // How much USD is required to purchase 1 ether unit, with 8 decimals
+	APY        *big.Int `json:"apy"`        // In ray
+}
+
+type StrategyStep struct {
+	Market   *MarketInfo `json:"market"`
+	IsSupply bool        `json:"isSupply"`
+	APY      *big.Int    `json:"apy"`
+	Amount   *big.Int    `json:"amount"`
+}
+
+type Strategy struct {
+	Steps []*StrategyStep `json:"steps"`
+	APY   *big.Int        `json:"apy"`
+}
